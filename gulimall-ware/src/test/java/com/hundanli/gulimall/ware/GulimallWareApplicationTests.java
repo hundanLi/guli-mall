626e9a5ddfe13510956a63a94295630565e44ab4
hundanLi <zulongli@qq.com>
2020-05-09 21:23:08 +0800

    项目结构初始化

---
+++ gulimall-ware/src/test/java/com/hundanli/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hundanli.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
